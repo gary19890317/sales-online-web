14fdec327ac18c7cc9444db0aca68f72a9a2c6ae
Guzman <703746@cognizant.com>
2020-12-01 20:47:56 -0600

    first commit

---
+++ src/main/java/com/sales/online/SalesOnlineWebApplication.java
@@ -0,0 +1,13 @@
+package com.sales.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesOnlineWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesOnlineWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sales/online/SalesOnlineWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sales.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesOnlineWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
